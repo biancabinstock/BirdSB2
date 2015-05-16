dd7f87361aac5e2230ec8121f0b5941cbd4ce5c8
Bianca Binstock <bianca.pr@gmail.com>
2015-05-16 17:38:00 -0400

    added empty detailViewController

---
+++ DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  BirdSB2
+//
+//  Created by Bianca Binstock on 2015-05-16.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
